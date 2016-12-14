18ccdc10d249578f0a7da761088050c593b0476f
Alex Tsui <alextsui05@gmail.com>
2016-12-14 11:50:18 -0800

    Remove styles from the audio file

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :song, styles: { medium: "700x500#", small: "350x250#" }
+	has_attached_file :song
   validates_attachment_content_type :song, content_type: /\Aaudio\/.*\Z/
 end
