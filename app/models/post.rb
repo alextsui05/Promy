43d9eb9731f8c852aa1444be1f957f77f7df55e6
Alex Tsui <alextsui05@gmail.com>
2016-12-14 09:58:02 -0800

    Allow audio file upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments
 	has_attached_file :song, styles: { medium: "700x500#", small: "350x250#" }
-  validates_attachment_content_type :song, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :song, content_type: /\Aaudio\/.*\Z/
 end
